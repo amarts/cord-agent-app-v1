09ea19745c27b408eb34ce271ece7b7bc0e2f6ab
Amar Tumballi <amar@dhiway.com>
2022-12-30 00:08:19 +0530

    add serto-schema create method
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- src/controller/schema.ts
+++ src/controller/schema.ts
@@ -4,6 +4,24 @@ import { getConnection } from "typeorm";
 import { Schema as SchemaCord } from "../cord/schema";
 import { Schema as SchemaEntity } from "../entity/Schema";
 
+function getSchemaObj(schema: any) {
+    let result = {
+	options: {},
+	schema: {
+	    '$schema': 'http://json-schema.org/draft-07/schema#',
+	    '$metadata': {},
+	    '$id': {},
+	    type: 'object',
+	    title: schema.title,
+	    description: schema.description,
+	    properties: schema.properties,
+	    required: schema.required
+	}
+    }
+
+    return result;
+}
+
 export class Schema {
   public static async create(req: express.Request, res: express.Response) {
     const data = req.body;
@@ -48,7 +66,92 @@ export class Schema {
     }
   }
 
-  public static async show(req: express.Request, res: express.Response) {
+    public static async create1(
+	req: express.Request,
+	res: express.Response
+    ) {
+	/* Check the POST input and get the API sorted out */
+	const data = req.body;
+
+	/* TODO: check for login */
+	const userId = req.header('X-UserId');
+
+	if (userId !== '7his15admin') {
+	    return res.status(403).json({error: "Unauthorized"})
+	}
+	
+	/* check for slug duplicate */
+	let currentSchema = await getConnection()
+            .getRepository(SchemaEntity)
+            .createQueryBuilder('s')
+            .where('s.slug = :url', { url: data.slug })
+            .andWhere('s.latest = :latest', { latest: true })
+            .getOne();
+
+	const schema = new SchemaEntity();
+	schema.slug = data.slug;
+	schema.category = data.category;
+	schema.description = data.description;
+	schema.icon = data.icon;
+	schema.title = data.name;
+	schema.versionStr = data.version;
+	schema.public = data.discoverable;
+	schema.jsonSchema = data.jsonSchema;
+	schema.ldContext = data.ldContext;
+	schema.ldContextPlus = data.ldContextPlus;
+	schema.forkOf = '';
+
+	if (currentSchema !== undefined) {
+            if (currentSchema.versionStr === data.version) {
+		res.status(400).json({
+                    error: 'Slug already present. Try without discoverable field, or provide different slug',
+		});
+		return;
+            }
+            schema.slug = currentSchema.slug;
+            schema.favoriteCount = currentSchema.favoriteCount;
+            schema.numForks = currentSchema.numForks;
+            schema.version = currentSchema.version! + 1;
+        }
+
+	const schemaObj = getSchemaObj(schema);
+	const response = await SchemaCord.anchor(schemaObj);
+	if (!response.schema) {
+	    return res.status(400).json({error: response.error});
+	}
+	schema.content = JSON.stringify(schemaObj);
+	schema.cordSchema = JSON.stringify(response.schema);
+        schema.cordBlock = response.block;
+	try {
+            await getConnection().transaction(
+		async (transactionalEntityManager) => {
+		    if (currentSchema) {
+			// TODO: provide another API to mark schema as non active
+			// Till that time, allow only latest schema to be used in
+			// #MARK creation.
+			currentSchema.latest = false;
+			await transactionalEntityManager.save(currentSchema);
+		    }
+		    
+		    const savedData = await transactionalEntityManager.save(
+			schema
+		    );
+		    if (savedData) {
+			res.status(201).json(savedData);
+		    } else {
+			res.status(500).json({
+			    error: 'Failed to create Schema',
+			});
+		    }
+		}
+	    );
+	} catch (err) {
+	    res.status(500).json({ error: 'Failed to create Schema' });
+	    return;
+	}
+    }
+    
+    public static async show(req: express.Request, res: express.Response) {
     try {
       const schema = await getConnection()
         .getRepository(SchemaEntity)
--- src/entity/Schema.ts
+++ src/entity/Schema.ts
@@ -28,6 +28,60 @@ export class Schema {
     @Column({nullable: true, default: null})
     revoked?: boolean;
 
+    @Column({ default: 'General' })
+    category?: string;
+
+    @Column({ default: '-' })
+    description?: string;
+
+    @Column({ default: null, nullable: true })
+    properties?: string;
+
+    @Column({ default: '' })
+    uniqueField?: string;
+
+    @Column({
+        type: 'text',
+        array: true,
+        default: null,
+        nullable: true,
+    })
+    requires?: string[];
+
+    @Column({ default: null, nullable: true })
+    versionStr?: string;
+
+    @Column({ default: 1 })
+    version?: number;
+
+    @Column({ default: null, nullable: true })
+    jsonSchema?: string;
+    @Column({ default: null, nullable: true })
+    ldContext?: string;
+    @Column({ default: null, nullable: true })
+    ldContextPlus?: string;
+
+    @Column({ default: true })
+    latest?: boolean;
+
+    @Column({ default: false })
+    public?: boolean;
+
+    @Column({ default: null, nullable: true })
+    icon?: string;
+
+    @Column({ default: null, nullable: true })
+    forkOf?: string;
+
+    @Column({ default: 0 })
+    favoriteCount?: number;
+
+    @Column({ default: 0 })
+    numForks?: number;
+
+    @Column({ default: null, nullable: true })
+    slug?: string; // this is common upon updates
+    
     @Column({type: 'text'})
     content?: string;
 
